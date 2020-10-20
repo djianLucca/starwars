23ac99fd99f69cca086cc7d4eb8c166ac9725ea5
Djian Stiegelmaier <djianlucca@outlook.com>
2020-10-20 19:45:54 -0300

    Removing unused dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
-import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from "./app.component";
 import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
@@ -22,7 +21,7 @@ import { ComponentsModule } from "./components/components.module";
     NgbModule,
     RouterModule,
     AppRoutingModule,
-    ToastrModule.forRoot()
+  
   ],
   declarations: [AppComponent, AdminLayoutComponent],
   providers: [],
