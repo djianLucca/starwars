803771e849b288f9c69b08f10575995c0947ae76
Djian Stiegelmaier <djianlucca@outlook.com>
2020-10-20 18:30:10 -0300

    Fixing the light/dark mode switch on mobile

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { HttpHeaders, HttpParams, HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class BaseService {
