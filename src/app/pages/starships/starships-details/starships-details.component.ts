4d6cf2948b07b5acfce32cfb8f03b9e9b51194be
Djian Stiegelmaier <djianlucca@outlook.com>
2020-10-21 15:18:59 -0300

    Finishing overall

--- src/app/pages/starships/starships-details/starships-details.component.ts
+++ src/app/pages/starships/starships-details/starships-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Starship } from 'src/app/classes/starship';
+import { FilmsService } from 'src/app/services/films.service';
+import { PeopleService } from 'src/app/services/people.service';
+import { StarshipsService } from 'src/app/services/starships.service';
 
 @Component({
   selector: 'app-starships-details',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarshipsDetailsComponent implements OnInit {
 
-  constructor() { }
+  public starship: Starship
+  public starshipImage: string
+
+  constructor(
+    private _activatedRouter: ActivatedRoute,
+    private _charactersService: PeopleService,
+    private _filmsService: FilmsService,
+    private _starshipsService: StarshipsService,
+  ) { }
 
   ngOnInit(): void {
+    this.getStarship()
   }
 
+  getStarship() {
+    this._activatedRouter.params.subscribe((params: any) => {
+      let starshipId = params['id']
+      if (starshipId) {
+        this._starshipsService.getById(starshipId)
+          .subscribe((res: any) => {
+            let allFilms = [];
+            let allCharacters = [];
+
+            res.films.map(filmUrl => {
+              let filmId = filmUrl.replace(/[^0-9]/g, '')
+              this._filmsService.getById(filmId).subscribe((film: any) => {
+                film.url = '/films/details/' + filmId
+                allFilms.push(film)
+              })
+            })
+            res.pilots.map(characterUrl => {
+              let characterId = characterUrl.replace(/[^0-9]/g, '')
+              return this._charactersService.getById(characterId).subscribe((character: any) => {
+                character.url = '/characters/details/' + characterId
+                allCharacters.push(character)
+              })
+            })
+
+            res.films = allFilms 
+            res.characters = allCharacters 
+
+            this.setStarshipPicture(res.url)
+
+            this.starship = res
+          })
+      }
+    })
+  }
+
+  setStarshipPicture(url) {
+    let starshipId = url.replace(/[^0-9]/g, '')
+    this.starshipImage =  `./assets/img/starships/${starshipId}.jpg`
+  }
 }
--- src/app/pages/vehicles/vehicles-details/vehicles-details.component.ts
+++ src/app/pages/vehicles/vehicles-details/vehicles-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Vehicle } from 'src/app/classes/vehicle';
+import { FilmsService } from 'src/app/services/films.service';
+import { PeopleService } from 'src/app/services/people.service';
+import { VehiclesService } from 'src/app/services/vehicles.service';
 
 @Component({
   selector: 'app-vehicles-details',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehiclesDetailsComponent implements OnInit {
 
-  constructor() { }
+  public vehicle: Vehicle
+  public vehicleImage: string
+
+  constructor(
+    private _activatedRouter: ActivatedRoute,
+    private _charactersService: PeopleService,
+    private _filmsService: FilmsService,
+    private _vehiclesService: VehiclesService,
+  ) { }
 
   ngOnInit(): void {
+    this.getVehicle()
+  }
+
+  getVehicle() {
+    this._activatedRouter.params.subscribe((params: any) => {
+      let vehicleId = params['id']
+      if (vehicleId) {
+        this._vehiclesService.getById(vehicleId)
+          .subscribe((res: any) => {
+            let allFilms = [];
+            let allCharacters = [];
+
+            res.films.map(filmUrl => {
+              let filmId = filmUrl.replace(/[^0-9]/g, '')
+              this._filmsService.getById(filmId).subscribe((film: any) => {
+                film.url = '/films/details/' + filmId
+                allFilms.push(film)
+              })
+            })
+            res.pilots.map(characterUrl => {
+              let characterId = characterUrl.replace(/[^0-9]/g, '')
+              return this._charactersService.getById(characterId).subscribe((character: any) => {
+                character.url = '/characters/details/' + characterId
+                allCharacters.push(character)
+              })
+            })
+
+            res.films = allFilms 
+            res.characters = allCharacters 
+
+            this.setVehiclePicture(res.url)
+
+            this.vehicle = res
+          })
+      }
+    })
+  }
+
+  setVehiclePicture(url) {
+    let vehicleId = url.replace(/[^0-9]/g, '')
+    this.vehicleImage =  `./assets/img/vehicles/${vehicleId}.jpg`
   }
 
 }
